2055dc4e3f355ffc851d2ee78953398db4252c8b
lsavila <luca.avila@edu.ufes.br>
2022-11-21 14:24:08 -0300

    Bora começar esse build.xml e umas coisas da main

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import Relatorios.*;
+
+import java.io.*;
+
+import Candidaturas.*;
+import Estaticos.*;
+import Exceptions.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        // PRIMEIRA PARTE: leitura dos arquivos de candidatos
+        SistemaEleitoral sistema = new SistemaEleitoral();
+        try {
+            BufferedReader buffRead = new BufferedReader(new FileReader("consulta_cand_2022/consulta_cand_2022_ES.csv"));
+            buffRead.close();
+        } catch (Exception e) {
+            System.out.println("Arquivo de candidatos não encontrado");
+        }
+        
+    }   
 }
