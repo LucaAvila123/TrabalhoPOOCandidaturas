fb66a27ce61d300fd003415f92541d00511bde39
Luca <91071096+LucaAvila123@users.noreply.github.com>
2022-12-04 09:31:49 -0300

    Delete src/Estaticos directory

--- src/Estaticos/Legenda.java
+++
@@ -1,8 +0,0 @@
-package Estaticos;
-
-public class Legenda {
-    public static boolean vaiPraLegenda(String situacao){
-        if(situacao.equals("Válido (legenda)")) return true;
-        return false;
-    }
-}
